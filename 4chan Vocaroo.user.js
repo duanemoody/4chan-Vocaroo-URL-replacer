14f1d513546cb48aa74c8970064f0afa7f6d01ed
Scott Steiner <nothingfinerthanscottsteiner@gmail.com>
2011-06-01 05:26:41 -0400

    1.0.2 - Added wmode transparent param to help with some Stylish theme issues. Also helps with scrolling and 420chan's menu system
    Signed-off-by: Scott Steiner <nothingfinerthanscottsteiner@gmail.com>

--- 4chan Vocaroo.user.js
+++ 4chan Vocaroo.user.js
@@ -9,8 +9,8 @@
 // @include			http://dis.4chan.org/read/*
 // @include			http://green-oval.net/cgi-board.pl/*
 // @include			http://suptg.thisisnotatrueending.com/archive/*
-// @version			1.0.1
-// @copyright		2010, Scott Steiner <#ScottSteiner@irc.rizon.net>
+// @version			1.0.2
+// @copyright		2010-2011, Scott Steiner <#ScottSteiner@irc.rizon.net>
 // @license			GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // ==/UserScript==
 
@@ -22,7 +22,7 @@ reDisSites		= /(?:dis.4chan.org)/
 reLinkedSites	= /(?:420chan.org|green-oval.net)/
 reMainSites		= /(?:boards.4chan.org|4chanarchive.org|archive.easymodo.net|suptg.thisisnotatrueending.com)/
 
-embedcode 		= '<object type="application/x-shockwave-flash" style="width: 148px; height: 44px" data="http://vocaroo.com/player.swf?playMediaID=$1&server=m1.vocaroo.com&autoplay=0""><param name="movie" value="http://vocaroo.com/player.swf?playMediaID=$1&server=m1.vocaroo.com&autoplay=0"></object><br />'
+embedcode 		= '<object type="application/x-shockwave-flash" style="width: 148px; height: 44px" data="http://vocaroo.com/player.swf?playMediaID=$1&server=m1.vocaroo.com&autoplay=0""><param name="wmode" value="transparent"><param name="movie" value="http://vocaroo.com/player.swf?playMediaID=$1&server=m1.vocaroo.com&autoplay=0"></object><br />'
 
 posts = document.getElementsByTagName("blockquote");
 for (i = 0; i < posts.length; i++) { 
