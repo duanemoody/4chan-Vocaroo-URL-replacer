47a02acf4e6e42003a5ac2d6239a194266587293
Scott Steiner <nothingfinerthanscottsteiner@gmail.com>
2011-06-01 05:25:54 -0400

    1.0b - Fixed CSS style bug - Improved support for boards which parse links
    Signed-off-by: Scott Steiner <nothingfinerthanscottsteiner@gmail.com>

--- 4chan Vocaroo.user.js
+++ 4chan Vocaroo.user.js
@@ -8,20 +8,20 @@
 // @include			http://boards.420chan.org/*
 // @include			http://dis.4chan.org/read/*
 // @include			http://suptg.thisisnotatrueending.com/archive/*
-// @version			1.0
+// @version			1.0b
 // @copyright		2010, Scott Steiner <#ScottSteiner@irc.rizon.net>
 // @license			GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // ==/UserScript==
 
 reDis			= /<a rel.*>.*?\?v=([a-zA-Z0-9_-]{11}).*?<\/a>(?:<br \/>|)/
-reLinked		= /<a href="(?:http.{3}|)(?:www.|)vocaroo.com\/\?media=.*" rel="nofollow">.*\?media=([a-zA-Z0-9]{17}).*?<\/a>/
+reLinked		= /<a href="(?:http.{3}|)(?:www.|)vocaroo.com\/\?media=.*?" rel="nofollow">(?:http.{3}|)(?:www.|)vocaroo.com\/\?media=([a-zA-Z0-9]{17}).*<\/a>/
 reMain			= /(?:http.{3}|)(?:www.|)vocaroo.com\/\?media=([a-zA-Z0-9]{17})(?:<br>|)/
 
 reDisSites		= /(?:dis.4chan.org)/
 reLinkedSites	= /(?:420chan.org)/
 reMainSites		= /(?:boards.4chan.org|4chanarchive.org|archive.easymodo.net|suptg.thisisnotatrueending.com)/
 
-embedcode 		= '<object type="application/x-shockwave-flash" style="width: 148; height: 44" data="http://vocaroo.com/player.swf?playMediaID=$1&server=m1.vocaroo.com&autoplay=0""><param name="movie" value="http://vocaroo.com/player.swf?playMediaID=$1&server=m1.vocaroo.com&autoplay=0"></object><br />'
+embedcode 		= '<object type="application/x-shockwave-flash" style="width: 148px; height: 44px" data="http://vocaroo.com/player.swf?playMediaID=$1&server=m1.vocaroo.com&autoplay=0""><param name="movie" value="http://vocaroo.com/player.swf?playMediaID=$1&server=m1.vocaroo.com&autoplay=0"></object><br />'
 
 posts = document.getElementsByTagName("blockquote");
 for (i = 0; i < posts.length; i++) { 
