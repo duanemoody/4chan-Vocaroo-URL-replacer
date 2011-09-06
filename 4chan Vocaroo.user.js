e37169b16da23ffd3fc85861eb377ab166952abc
Scott Steiner <nothingfinerthanscottsteiner@gmail.com>
2011-09-05 22:30:07 -0400

    2.0 - Added duplicate object removal - Added limit to total number of embedded objects in a thread - Added support for easymodo, intallgentoo, 2chan, 7chan and 4chon - Removed support for green-oval, which shut down - Expanded settings (br before/after embed) - Supports 4chan X's thread update feature
    Signed-off-by: Scott Steiner <nothingfinerthanscottsteiner@gmail.com>

--- 4chan Vocaroo.user.js
+++ 4chan Vocaroo.user.js
@@ -1,1 +1,117 @@
-// ==UserScript==// @name			4chan Vocaroo URL replacer// @namespace		ScottSteiner@irc.rizon.net// @description		Turns plaintext vocaroo URLs into embedded objects// @icon			https://github.com/ScottSteiner/4chan-Vocaroo-URL-replacer/raw/master/icon.jpg// @version			1.1// @copyright		2010-2011, Scott Steiner <#ScottSteiner@irc.rizon.net>// @license			GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html// @include			http://4chanarchive.org/brchive/*// @include			http://archive.easymodo.net/cgi-board.pl/*// @include			http://archive.no-ip.org/*// @include			http://boards.4chan.org/*// @include			http://boards.420chan.org/*// @include			http://chanarchive.org/*// @include			http://www.chanarchive.org/*// @include			http://dis.4chan.org/read/*// @include			http://green-oval.net/cgi-board.pl/*// @include			http://suptg.thisisnotatrueending.com/archive/*//// @match			http://4chanarchive.org/brchive/*// @match			http://archive.easymodo.net/cgi-board.pl/*// @match			http://archive.no-ip.org/*// @match			http://boards.4chan.org/*// @match			http://boards.420chan.org/*// @match			http://chanarchive.org/*// @match			http://www.chanarchive.org/*// @match			http://dis.4chan.org/read/*// @match			http://green-oval.net/cgi-board.pl/*// @match			http://suptg.thisisnotatrueending.com/archive/*// ==/UserScript==limit			= 5reDis			= /<a rel.*>.*?\?v=([a-zA-Z0-9_-]{11}).*?<\/a>(?:<br \/>|)/reLinked		= /<a href="(?:http.{3}|)(?:www.|)vocaroo.com\/\?media=.*?"(?: rel="nofollow"|)>(?:http.{3}|)(?:www.|)vocaroo.com\/\?media=([a-zA-Z0-9]{17}).*<\/a>(?:<br>|)/greMain			= /(?:http.{3}|)(?:www.|)vocaroo.com\/\?media=([a-zA-Z0-9]{17})(?:<br>|)/gembedcode 		= '<object type="application/x-shockwave-flash" style="width: 148px; height: 44px" data="http://vocaroo.com/player.swf?playMediaID=$1&server=m1.vocaroo.com&autoplay=0""><param name="movie" value="http://vocaroo.com/player.swf?playMediaID=$1&server=m1.vocaroo.com&autoplay=0"></object><br />'switch(document.domain) {	case "archive.no-ip.org" :	case "boards.420chan.org" :	case "green-oval.net" :					reCode = reLinked;	break;	case "4chanarchive.org" :	case "archive.easymodo.net" :	case "boards.4chan.org" :	case "chanarchive.org":	case "suptg.thisisnotatrueending.com" :	reCode = reMain;	break;	case "dis.4chan.org" :					reCode = reDis;		break;}posts = document.getElementsByTagName("blockquote");for (i = 0; i < posts.length; i++) { 	l = limit;	temp = posts[i].innerHTML.replace(reCode, function (v) {return l-- > 0 ? v.replace(reCode,embedcode) : v;});	if (temp != posts[i].innerHTML) { posts[i].innerHTML = temp }}
+/*
+    4chan Vocaroo URL Replacer
+    Copyright (C) 2010-2011 ScottSteiner (nothingfinerthanscottsteiner@gmail.com)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// ==UserScript==
+// @name			4chan Vocaroo URL Replacer
+// @namespace		http://about.me/ScottSteiner
+// @id				Vocaroo@ScottSteiner
+// @author			Scott Steiner <nothingfinerthanscottsteiner@gmail.com> http://about.me/ScottSteiner
+// @description		Turns plaintext Vocaroo URLs into embedded objects
+// @version			2.0
+// @copyright		2010-2011, Scott Steiner <nothingfinerthanscottsteiner@gmail.com>
+// @license			GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
+// @icon			https://github.com/ScottSteiner/4chan-Vocaroo-URL-Replacer/raw/master/icon.jpg
+// @homepage		https://github.com/ScottSteiner/4chan-Vocaroo-URL-Replacer
+// @supportURL		https://github.com/ScottSteiner/4chan-Vocaroo-URL-Replacer/issues
+// @updateURL		https://userscripts.org/scripts/source/89348.meta.js
+// @screenshot		https://github.com/ScottSteiner/4chan-Vocaroo-URL-Replacer/raw/master/screenshot.jpg
+// @priority		1
+//
+// @include			http://*.2chan.net/*
+// @include			http://7chan.org/*
+// @include			http://*.4chon.net/*
+// @include			http://4chanarchive.org/brchive/*
+// @include			http://archive.easymodo.net/*
+// @include			http://archive.installgentoo.net/cgi-board.pl/*
+// @include			http://archive.no-ip.org/*
+// @include			http://boards.4chan.org/*
+// @include			http://boards.420chan.org/*
+// @include			http://chanarchive.org/*
+// @include			http://dis.4chan.org/*
+// @include			http://*.krautchan.net/*
+// @include			http://operatorchan.org/*
+// @include			http://suptg.thisisnotatrueending.com/archive/*
+//
+// @match			http://*.2chan.net/*
+// @match			http://*.4chon.net/*
+// @match			http://7chan.org/*
+// @match			http://4chanarchive.org/brchive/*
+// @match			http://archive.easymodo.net/*
+// @match			http://archive.installgentoo.net/cgi-board.pl/*
+// @match			http://archive.no-ip.org/*
+// @match			http://boards.4chan.org/*
+// @match			http://boards.420chan.org/*
+// @match			http://chanarchive.org/*
+// @match			http://dis.4chan.org/*
+// @match			http://*.krautchan.net/*
+// @match			http://operatorchan.org/*
+// @match			http://suptg.thisisnotatrueending.com/archive/*
+// ==/UserScript==
+
+(function () {
+	"use strict";
+	var i, embedArray = new Array(0), embedCur = 0, embedTotal = 0, re = {}, settings, sites = {}, siteArray;
+	settings = {
+		filter: {
+			limitPer:		5,
+			limitTotal:		-1,
+			ignoreDupes:	true
+		},
+		embed: {
+			brBefore:		false,
+			brAfter:		false
+		}
+	};
+	settings.embed.code = (settings.embed.brBefore ? '<br />' : '') + '<object type="application/x-shockwave-flash" style="width: 148px; height: 44px" data="http://vocaroo.com/player.swf?playMediaID=$1&server=m1.vocaroo.com&autoplay=0"><"param name="movie" value="http://vocaroo.com/player.swf?playMediaID=$1&server=m1.vocaroo.com&autoplay=0"</object>' + (settings.embed.brAfter ? '<br />' : '');
+	
+	re.plaintext	= /(?:http.{3}|)(?:www.|)vocaroo.com\/\?media=([\w]{17})(?:<br>|)/g
+	re.linked		= /<a href="(?:http.{3}|)(?:www.|)vocaroo.com\/\?media=.*?"(?: rel="nofollow"|)>(?:http.{3}|)(?:www.|)vocaroo.com\/\?media=([\w]{17}).*<\/a>(?:<br>|)/g
+
+	sites.plaintext	= [/(?:\w*\.2chan\.net|4chanarchive\.org|boards\.4chan\.org|chanarchive\.org|krautchan\.net|suptg\.thisisnotatrueending\.com)/, 'blockquote', re.plaintext];
+	sites.linkedBQ	= [/(?:4chan\.org|archive\.easymodo\.net|archive\.installgentoo\.net|archive\.no-ip\.org|boards\.420chan\.org|operatorchan\.org)/, 'blockquote', re.linked];
+	sites.linkedDiv	= [/(?:7chan\.org)/, 'div', re.linked];
+	sites.linkedP	= [/(?:4chon\.net)/, 'p', re.linked];
+
+	for (i in sites) { if (sites.hasOwnProperty(i)) { if (sites[i][0].exec(document.domain)) { siteArray = sites[i]; break; } } }
+	
+	function embedPost(match) {
+		var embedURL;
+		embedTotal++;
+		embedCur++;
+		embedURL = match.replace(siteArray[2], '$1');
+		if ((embedArray.indexOf(embedURL) > -1) && (settings.filter.ignoreDupes)) { return ''; }
+		if (((settings.filter.limitPer < 0) || (embedCur <= settings.filter.limitPer)) && ((settings.filter.limitTotal < 0) || (embedTotal <= settings.filter.limitTotal))) { 
+			embedArray.push(embedURL);
+			return settings.embed.code.replace('$1', embedURL);
+		} else { return match; }
+	}
+				
+	function embed(n) {
+		if ((typeof k == 'number') && n.target.nodeType !== 1) { return; }
+		var l, posts, temp;
+		posts = document.getElementsByTagName(siteArray[1]);
+		for (l = 0; l < posts.length; l++) { 
+			embedCur = 0;
+			embedArray.length = 0;
+			temp = posts[l].innerHTML.replace(siteArray[2], embedPost);
+			if (temp !== posts[l].innerHTML) { posts[l].innerHTML = temp; }
+		}
+	}
+	document.addEventListener('DOMNodeInserted', embed, true);
+	embed();
+}());
